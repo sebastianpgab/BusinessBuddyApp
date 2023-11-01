23911d0a5dd9f4d2c2d52998b2b191a3e52d659b
sebastianpgab <sebastianpgab@gmail.com>
2023-11-01 23:59:02 +0100

    Add method to update Product and make Mug-entity property nullable

--- BusinessBuddyApp/Entities/Mug.cs
+++ BusinessBuddyApp/Entities/Mug.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Material { get; set; }
-        public double Capacity { get; set; }
-        public bool IsMicrowaveSafe { get; set; }
-        public bool IsDishwasherSafe { get; set; }
+        public double? Capacity { get; set; }
+        public bool? IsMicrowaveSafe { get; set; }
+        public bool? IsDishwasherSafe { get; set; }
         public int ProductId { get; set; } // Klucz obcy
 
     }
--- BusinessBuddyApp/Services/ProductService.cs
+++ BusinessBuddyApp/Services/ProductService.cs
@@ -18,8 +18,8 @@ namespace BusinessBuddyApp.Services
 
         public async Task<IEnumerable<Product>> GetAll()
         {
-            var products = _dbContext.Products.Include(p => p.Clothe).Include(w => w.Mug).Include(c => c.Other);
-            if(products.Any())
+            var products = await _dbContext.Products.Include(p => p.Clothe).Include(w => w.Mug).Include(c => c.Other).ToListAsync();
+            if (products.Any())
             {
                 return products;
             }
@@ -34,6 +34,56 @@ namespace BusinessBuddyApp.Services
                 return product;
             }
             throw new ArgumentNullException("Product not found");
+        }
+
+        public async Task<Product> Update(Product updatedProduct, int id)
+        {       
+            switch (updatedProduct.ProductType)
+            {
+                case "Mug":
+
+                    var mugProduct = await _dbContext.Mugs.FirstOrDefaultAsync(p => p.Id == id);
+
+                    if(mugProduct is not null)
+                    {
+                        if (updatedProduct.Mug.Material is not null) { mugProduct.Material = updatedProduct.Mug.Material; }
+                        if (updatedProduct.Mug.Capacity is not null) { mugProduct.Capacity = updatedProduct.Mug.Capacity; }
+                        if (updatedProduct.Mug.IsMicrowaveSafe is not null) { mugProduct.IsMicrowaveSafe = updatedProduct.Mug.IsMicrowaveSafe;}
+                        if (updatedProduct.Mug.IsDishwasherSafe is not null) { mugProduct.IsDishwasherSafe = updatedProduct.Mug.IsDishwasherSafe;}
+                    }
+                    break;
+
+                case "Clothe":
+
+                    var clotheProduct = await _dbContext.Clothes.FirstOrDefaultAsync(p => p.Id == id);
+
+                    if (clotheProduct is not null)
+                    {
+                        if (updatedProduct.Clothe.Size is not null) { clotheProduct.Size = updatedProduct.Clothe.Size; }
+                        if (updatedProduct.Clothe.Gender is not null) {clotheProduct.Gender = updatedProduct.Clothe.Gender; }
+                        if (updatedProduct.Clothe.Brand is not null) {clotheProduct.Brand = updatedProduct.Clothe.Brand; }
+                        if (updatedProduct.Clothe.Style is not null) { clotheProduct.Style = updatedProduct.Clothe.Style; }
+                    }
+                    break;
+
+                case "Orther":
+
+                    var otherProduct = await _dbContext.Others.FirstOrDefaultAsync(p => p.Id == id);
+
+                    if(otherProduct is not null)
+                    {
+                        if (updatedProduct.Other.Description is not null) { otherProduct.Description = updatedProduct.Other.Description;}
+                    }
+                    break;
+
+                default:
+                    throw new Exception($"Unknown product type: {updatedProduct.ProductType}.");
+                                           
+            }
+
+            await _dbContext.SaveChangesAsync();
+            return updatedProduct;
+            
         }
 
 
