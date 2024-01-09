263a14a84af3d5d99db2e59726f1fcb3d5fb42b2
sebastianpgab <sebastianpgab@gmail.com>
2024-01-09 23:54:13 +0100

    Add nullalbe Client in Order entity

--- BusinessBuddyApp/Entities/Order.cs
+++ BusinessBuddyApp/Entities/Order.cs
@@ -6,7 +6,7 @@
         public int ClientId { get; set; }
         public int? OrderDetailId { get; set; }
         public int? InvoiceId { get; set; }
-        public virtual Client Client {get; set;}
+        public virtual Client? Client {get; set;}
         public virtual OrderDetail? OrderDetail { get; set; }
         public virtual Invoice? Invoice { get; set; }
 
